206bac430bb06fb384c6820e13e72ac4a1f10a24
qxf2 <mak@qxf2.com>
2020-10-19 15:39:12 +0530

    Message cleaner (#1)
    * Added a preprocessor for the message
    
    * Added a preprocessor for the message
    
    Co-authored-by: Ubuntu <ubuntu@ip-172-26-2-46.ap-south-1.compute.internal>

--- is_pto/is_pto.py
+++ is_pto/is_pto.py
@@ -4,30 +4,16 @@ Figure out if a message sent is a PTO or not
 import os
 import pickle
 import re
+import is_pto.preprocess_message as message_cleaner
 
 CURRENT_DIRECTORY = os.path.dirname(__file__)
 MODEL_FILENAME = os.path.join(CURRENT_DIRECTORY, 'pto_classifier.pickle')
 QUERIES_FILENAME = os.path.join(CURRENT_DIRECTORY,'queries.log')
 
-def get_clean_message(message):
-    "Clean the message"
-    cleaned_message = re.sub(r'<.*</.*?>', '', message) #quoted message
-    cleaned_message = re.sub(r'\B@\w+', '', cleaned_message) #@mentions
-    cleaned_message = re.sub(r'&.*?;', '', cleaned_message) #encoded strings
-    cleaned_message = re.sub(r'^(\s)*$\n', '', cleaned_message) #emtpy lines
-    cleaned_message = cleaned_message.replace('<legacyquote>', '')
-    cleaned_message = cleaned_message.replace(',', '')
-    cleaned_message = cleaned_message.replace('.', '')
-    cleaned_message = cleaned_message.replace('"', '')
-    cleaned_message = cleaned_message.replace("'", '')
-    cleaned_message = cleaned_message.replace('\\', '')
-
-    return cleaned_message
-
 def is_this_a_pto(message):
     "Return a prediction of whether this is a PTO or not"
     model = pickle.load(open(MODEL_FILENAME, 'rb'))
-    message = get_clean_message(message)
+    message = message_cleaner.get_clean_message(message)
     with open(QUERIES_FILENAME,'a') as file_handler:
         file_handler.write(message + "\n")
 
---
+++ is_pto/preprocess_message.py
@@ -0,0 +1,44 @@
+"""
+Preprocess incoming message to match what the training model uses
+a) Clean the unwanted portions of the Skype message and SQS formatting
+b) Mimic the training model - remove stop words and stem the words
+"""
+import re
+from nltk.corpus import stopwords
+from nltk.stem import SnowballStemmer
+NOT_STOP_WORDS = ['not']
+ADD_STOP_WORDS = ['today', 'tomorrow', 'yesterday']
+
+def clean_sqs_skype_formatting(message):
+    "Clean up unwanted Skype and SQS formatting"
+    cleaned_message = re.sub(r'<.*</.*?>', '', message) #quoted message
+    cleaned_message = re.sub(r'\B@\w+', '', cleaned_message) #@mentions
+    cleaned_message = re.sub(r'&.*?;', '', cleaned_message) #encoded strings
+    cleaned_message = re.sub(r'^(\s)*$\n', '', cleaned_message) #emtpy lines
+    cleaned_message = cleaned_message.replace('<legacyquote>', '')
+    cleaned_message = cleaned_message.replace(',', ' ')
+    cleaned_message = cleaned_message.replace('.', ' ')
+    cleaned_message = cleaned_message.replace('"', ' ')
+    cleaned_message = cleaned_message.replace("'", ' ')
+    cleaned_message = cleaned_message.replace('\\', ' ')
+
+    return cleaned_message
+
+def preprocess_message(message):
+    "Preprocess the message"
+    stemmer = SnowballStemmer('english')
+    words = stopwords.words("english")
+    for word in NOT_STOP_WORDS:
+        words.remove(word)
+    for word in ADD_STOP_WORDS:
+        words.append(word)
+    clean_message = " ".join([stemmer.stem(i) for i in re.sub("[^a-zA-Z]", " ", message).split() if i not in words]).lower()
+
+    return clean_message
+
+def get_clean_message(message):
+    "Clean the message"
+    message = clean_sqs_skype_formatting(message)
+    message = preprocess_message(message)
+
+    return message
