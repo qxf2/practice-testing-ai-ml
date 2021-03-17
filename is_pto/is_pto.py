86992f84074d8ad803ee00d5011cdcbc045ff198
qxf2 <mak@qxf2.com>
2021-03-17 18:53:38 +0530

    Udpated model, improved preprocessing (#4)

--- is_pto/is_pto.py
+++ is_pto/is_pto.py
@@ -12,9 +12,14 @@ QUERIES_FILENAME = os.path.join(CURRENT_DIRECTORY,'queries.log')
 
 def is_this_a_pto(message):
     "Return a prediction of whether this is a PTO or not"
+    answer = 0
     model = pickle.load(open(MODEL_FILENAME, 'rb'))
-    message = message_cleaner.get_clean_message(message)
-    with open(QUERIES_FILENAME,'a') as file_handler:
-        file_handler.write(message + "\n")
+    if len(message.strip().split()) == 1:
+        answer = 0
+    else:
+        message = message_cleaner.get_clean_message(message)
+        with open(QUERIES_FILENAME,'a') as file_handler:
+            file_handler.write(message + "\n")
+        answer = model.predict([message])[0]
 
-    return model.predict([message])[0]
+    return answer
