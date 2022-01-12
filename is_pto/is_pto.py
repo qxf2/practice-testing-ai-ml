a7c0b5dfdba024bc7f881cc90de1292e98025a8c
Qxf2 <mak@qxf2.com>
2022-01-12 18:50:54 +0530

    Refactored code to make is_pto a better unit testing example

--- is_pto/is_pto.py
+++ is_pto/is_pto.py
@@ -10,16 +10,24 @@ CURRENT_DIRECTORY = os.path.dirname(__file__)
 MODEL_FILENAME = os.path.join(CURRENT_DIRECTORY, 'pto_classifier.pickle')
 QUERIES_FILENAME = os.path.join(CURRENT_DIRECTORY,'queries.log')
 
+def get_model():
+    "Return the model"
+    return pickle.load(open(MODEL_FILENAME, 'rb'))
+
+def store_message(message):
+    "Store the input message to file for analysis at a later time"
+    with open(QUERIES_FILENAME,'a') as file_handler:
+        file_handler.write(message + "\n")
+
 def is_this_a_pto(message):
     "Return a prediction of whether this is a PTO or not"
     answer = 0
-    model = pickle.load(open(MODEL_FILENAME, 'rb'))
+    model = get_model()
     if len(message.strip().split()) == 1:
         answer = 0
     else:
         message = message_cleaner.get_clean_message(message)
-        with open(QUERIES_FILENAME,'a') as file_handler:
-            file_handler.write(message + "\n")
+        store_message(message)
         answer = model.predict([message])[0]
 
     return answer
